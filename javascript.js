2d9183ac2353edbc5e4b493c330072180c00edd6
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-01-13 18:17:31 -0700

    trying to fix resume download

--- javascript.js
+++ javascript.js
@@ -2,5 +2,13 @@ $(document).ready(function(){
 
     $('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
 
-
+    $.ajax({
+        url: "https://formsubmit.co/ajax/your@email.com",
+        method: "POST",
+        data: {
+            name: "FormSubmit",
+            message: "I'm from Devro LABS"
+        },
+        dataType: "json"
+    });
 })
